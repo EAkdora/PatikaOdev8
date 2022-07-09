2b4545f0f4abb0464186f252167613bac3a38a9d
EAkdora <108339327+EAkdora@users.noreply.github.com>
2022-07-09 19:25:09 +0300

    Create Soru3.sql
    Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.

---
+++ Soru3.sql
@@ -0,0 +1,35 @@
+UPDATE employee
+SET 
+    name = 'Ismail Kaymaz',
+    birthdate = '2010-10-11',
+    email = 'ismail@kaymaz.com'
+
+WHERE id = 1;
+UPDATE employee
+SET 
+    name = 'Osman Ossuran',
+    birthdate = '1990-01-23',
+    email = 'osman@ossuran.com'
+
+WHERE id = 2;
+UPDATE employee
+SET 
+    name = 'Dr. Kaya Bilir',
+    birthdate = '1980-07-01',
+    email = 'drkaya@bilir.com'
+
+WHERE id = 3;
+UPDATE employee
+SET 
+    name = 'Satılmış Portakal',
+    birthdate = '1970-05-21',
+    email = 'satilmis@portakal.com'
+
+WHERE id = 4;
+UPDATE employee
+SET 
+    name = 'Cihan Devlet',
+    birthdate = '1453-05-23',
+    email = 'cihan@devlet.com'
+
+WHERE id = 5;
